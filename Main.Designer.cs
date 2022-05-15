ff82e96adfe3a15fee41dbeb3465dff643f323e9
LadaTopor <hetag.kojiev@mail.ru>
2022-05-15 18:33:22 +0300

    newlvl

---
+++ Main.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace StegProject
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Enrypt = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Decrypt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Enrypt
+            // 
+            this.Enrypt.Location = new System.Drawing.Point(12, 12);
+            this.Enrypt.Name = "Enrypt";
+            this.Enrypt.Size = new System.Drawing.Size(75, 23);
+            this.Enrypt.TabIndex = 0;
+            this.Enrypt.Text = "Записать ";
+            this.Enrypt.UseVisualStyleBackColor = true;
+            this.Enrypt.Click += new System.EventHandler(this.Enrypt_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(669, 352);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Decrypt
+            // 
+            this.Decrypt.Location = new System.Drawing.Point(110, 12);
+            this.Decrypt.Name = "Decrypt";
+            this.Decrypt.Size = new System.Drawing.Size(97, 23);
+            this.Decrypt.TabIndex = 2;
+            this.Decrypt.Text = "Прочитать";
+            this.Decrypt.UseVisualStyleBackColor = true;
+            this.Decrypt.Click += new System.EventHandler(this.Decrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 405);
+            this.Controls.Add(this.Decrypt);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Enrypt);
+            this.Name = "Form1";
+            this.Text = "Стеганография";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Enrypt;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Decrypt;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,209 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace StegProject
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        const int ENCRYP_PESENT_SIZE = 1;
+        const int ENCRYP_TEXT_SIZE = 3;
+        const int ENCRYP_TEXT_MAX_SIZE = 999;
+
+
+
+
+        /* Открыть файл для шифрования */
+        private void Enrypt_Click(object sender, EventArgs e)
+        {
+            string FilePic;
+            string FileText;
+            OpenFileDialog dPic = new OpenFileDialog();
+            dPic.Filter = "Файлы изображений (*.bmp)|*.bmp|Все файлы (*.*)|*.*";
+            if (dPic.ShowDialog() == DialogResult.OK)
+            {
+                FilePic = dPic.FileName;
+            }
+            else
+            {
+                FilePic = "";
+                return;
+            }
+
+            FileStream rFile;
+            rFile = new FileStream(FilePic, FileMode.Open); //открываем поток
+            Bitmap bPic = new Bitmap(rFile);
+
+            OpenFileDialog dText = new OpenFileDialog();
+            dText.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (dText.ShowDialog() == DialogResult.OK)
+            {
+                FileText = dText.FileName;
+            }
+            else
+            {
+                FileText = "";
+                return;
+            }
+
+            FileStream rText;
+
+            rText = new FileStream(FileText, FileMode.Open); //открываем поток текстовым файлом
+            
+            BinaryReader bText = new BinaryReader(rText, Encoding.ASCII);
+
+            List<byte> bList = new List<byte>();
+            while (bText.PeekChar() != -1) { //считали весь текстовый файл для шифрования в лист байт
+                bList.Add(bText.ReadByte());
+            }
+            int CountText = bList.Count; // в CountText - количество в байтах текста, который нужно закодировать
+            bText.Close();
+            rFile.Close();
+
+            //проверям, что размер не выходит за рамки максимального, поскольку для хранения размера используется
+            //ограниченное количество байт
+            if (CountText > (ENCRYP_TEXT_MAX_SIZE - ENCRYP_PESENT_SIZE - ENCRYP_TEXT_SIZE)) {
+                MessageBox.Show("Размер текста велик для данного алгоритма, уменьшите размер", "Информация", MessageBoxButtons.OK);
+                return;
+            }
+
+            //проверяем, поместится ли исходный текст в картинке
+            if (CountText > (bPic.Width * bPic.Height)) {
+                MessageBox.Show("Выбранная картинка мала для размещения выбранного текста", "Информация", MessageBoxButtons.OK);
+                return;
+            }
+
+            //проверяем, может быть картинка уже зашифрована
+            if (Cryption.isEncryption(bPic))
+            {
+                MessageBox.Show("Файл уже зашифрован", "Информация", MessageBoxButtons.OK);
+                return;
+            }
+
+            Cryption.SetCryption(bPic);
+            //то есть в первом пикселе будет символ /, который говорит о том, что картика зашифрована
+
+            Cryption.WriteCountText(CountText, bPic); //записываем количество символов для шифрования
+
+            Cryption.Enrypt(bPic, bList); // Кодируем сообщение
+            pictureBox1.Image = bPic;
+
+            String sFilePic;
+            SaveFileDialog dSavePic = new SaveFileDialog();
+            dSavePic.Filter = "Файлы изображений (*.bmp)|*.bmp|Все файлы (*.*)|*.*";
+            if (dSavePic.ShowDialog() == DialogResult.OK)
+            {
+                sFilePic = dSavePic.FileName;
+            }
+            else
+            {
+                sFilePic = "";
+                return;
+            };
+
+            FileStream wFile;
+            wFile = new FileStream(sFilePic, FileMode.Create); //открываем поток на запись результатов
+
+            bPic.Save(wFile, System.Drawing.Imaging.ImageFormat.Bmp);
+            wFile.Close(); //закрываем поток
+        }
+
+        /*Открыть файл для дешифрования */
+        private void Decrypt_Click(object sender, EventArgs e)
+        {
+            string FilePic;
+            OpenFileDialog dPic = new OpenFileDialog();
+            dPic.Filter = "Файлы изображений (*.bmp)|*.bmp|Все файлы (*.*)|*.*";
+            if (dPic.ShowDialog() == DialogResult.OK)
+            {
+                FilePic = dPic.FileName;
+            }
+            else
+            {
+                FilePic = "";
+                return;
+            }
+
+            FileStream rFile;
+            rFile = new FileStream(FilePic, FileMode.Open); //открываем поток
+
+            Bitmap bPic = new Bitmap(rFile);
+            if (!Cryption.isEncryption(bPic)) {
+                MessageBox.Show("В файле нет зашифрованной информации", "Информация", MessageBoxButtons.OK);
+                rFile.Close();
+                return;
+            }
+
+            int countSymbol = Cryption.ReadCountText(bPic); //считали количество зашифрованных символов
+            byte[] message = new byte[countSymbol];
+            int index = 0;
+            bool st = false;
+            for (int i = ENCRYP_TEXT_SIZE + 1; i < bPic.Width; i++) {
+                for (int j = 0; j < bPic.Height; j++) {
+                    Color pixelColor = bPic.GetPixel(i, j);
+                    if (index == message.Length) {
+                        st = true;
+                        break;
+                    }
+                    BitArray colorArray = Bits.ByteToBit(pixelColor.R);
+                    BitArray messageArray = Bits.ByteToBit(pixelColor.R); ;
+                    messageArray[0] = colorArray[0];
+                    messageArray[1] = colorArray[1];
+
+                    colorArray = Bits.ByteToBit(pixelColor.G);
+                    messageArray[2] = colorArray[0];
+                    messageArray[3] = colorArray[1];
+                    messageArray[4] = colorArray[2];
+
+                    colorArray = Bits.ByteToBit(pixelColor.B);
+                    messageArray[5] = colorArray[0];
+                    messageArray[6] = colorArray[1];
+                    messageArray[7] = colorArray[2];
+                    message[index] = Bits.BitToByte(messageArray);
+                    index++;
+                }
+                if (st) {
+                    break;
+                }
+            }
+            string strMessage = Encoding.GetEncoding(1251).GetString(message);
+
+            string sFileText;
+            SaveFileDialog dSaveText = new SaveFileDialog();
+            dSaveText.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (dSaveText.ShowDialog() == DialogResult.OK)
+            {
+                sFileText = dSaveText.FileName;
+            }
+            else
+            {
+                sFileText = "";
+                rFile.Close();
+                return;
+            };
+
+            FileStream wFile;
+            wFile = new FileStream(sFileText, FileMode.Create); //открываем поток на запись результатов
+            rFile.Close();
+
+            StreamWriter wText = new StreamWriter(wFile, Encoding.Default);
+            wText.Write(strMessage);
+            MessageBox.Show("Текст записан в файл", "Информация", MessageBoxButtons.OK);
+            wText.Close();
+            wFile.Close(); //закрываем поток
+            rFile.Close();
+        }
+    }
+}
